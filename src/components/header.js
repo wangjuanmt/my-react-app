a2c877867ce3302bdbc079bf674794301d4c8747
wangjuanmt <472846889@qq.com>
2019-02-11 19:02:27 +0800

    使用html,css做navigate，logo图片显示长宽比失调

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,19 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="fixedHeaderContainer">
-        <div className="headerWrapper wrapper">
-          <header>
-            <a href="/">
-              <img className="logo" src="./src/images/logo.svg" alt="Home"/>
-              <h2 className="headerTitleWithLogo">Amuos</h2>
-            </a>
-            <div className="navigationWrapper navigationSlider">
-              <nav className="slidingNav">
-                <ul className="nav-site nav-site-internal">
-                  <li className=""><a href="./music.html" target="_self">Music</a>
-                  </li>
-                  <li className=""><a href="./movie.html" target="_self">Movie</a>
-                  </li>
-                  <li className=""><a href="./Book" target="_self">Book</a>
-                  </li>
-                  <li className="navSearchWrapper reactNavSearchWrapper">
-                    <input type="text" id="search_input_react" placeholder="Search" title="Search"/>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </header>
-        </div>
-      </div>
+      <ul>
+        {/* <li><a class="active" href="#">Home</a></li> */}
+        <li>
+          <a href="#">
+-              <img src="./src/images/logo.svg"/>
+-         </a>
+        </li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Movie</a></li>
+        <li><a href="#">Music</a></li>
+        <li><a href="#">Book</a></li>
+        <li><a href="#">About</a></li>
+    </ul>
     );
   }
 }
